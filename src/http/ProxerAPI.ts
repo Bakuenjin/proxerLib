718035832856874d6f96edc78a9bc4d1a15adbde
Julian Zameit <julian.zameit.96@gmail.com>
2019-10-23 12:45:18 +0200

    added descriptions to proxer api settings

--- src/http/ProxerAPI.ts
+++ src/http/ProxerAPI.ts
@@ -3,11 +3,27 @@ import HttpClientConfiguration from "./models/HttpClientConfiguration";
 import HttpClient from "./models/HttpClient";
 import UserAPI from "./api/UserAPI";
 
-type ApiSettings = { 
+type ApiSettings = {
+    /**
+     * The key that authorizes this app to communicate with the proxer API.
+     */ 
     apiKey?: string
+    /**
+     * A token to communicate with the proxer API as a user.
+     */
     userToken?: string
+    /**
+     * In test mode, no API key is required, but the responses are all predefined.
+     */
     testMode?: boolean
+    /**
+     * It is recommended to define a custom user agent for your app to better identify it.
+     */
     customUserAgent?: string
+    /**
+     * For testing purposes, a custom http client can be specified.
+     * It should implement the `HttpClient` interface (when using typescript).
+     */
     customHttpClient?: HttpClient
 }
 
