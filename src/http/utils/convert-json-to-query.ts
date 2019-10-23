1832aa72133755e693f44c3a2aff35df950a0452
Julian Zameit <julian.zameit.96@gmail.com>
2019-10-23 11:57:37 +0200

    json-to-query now filters undefined elements

--- src/http/utils/convert-json-to-query.ts
+++ src/http/utils/convert-json-to-query.ts
@@ -1,8 +1,10 @@
 /**
  * Converts the first layer of any JS object into a query string.
+ * It skips all undefined values.
  */
 export default function convertJsonToQuery(data: { [key: string]: any} ): string {
     const keys = Object.keys(data)
-    const queryData = keys.map(key => `${key}=${data[key]}`)
+    const filteredKeys = keys.filter(key => (typeof data[key] !== 'undefined'))
+    const queryData = filteredKeys.map(key => `${key}=${data[key]}`)
     return queryData.join('&')
 }
